4040664138ee7f78255f565d3c217f085382f4eb
wutai <1615140542@qq.com>
2020-08-18 17:19:05 +0800

    index redirect to login page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,7 +1,6 @@
 import App from './pages/App';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
 import EditorPage from './pages/EditorPage';
 import LogInPage from './pages/LogInPage';
 import ControlPanelPage from './pages/ControlPanelPage';
@@ -13,7 +12,7 @@ export default function Routes() {
 				<Route path="/login" children={<LogInPage />} />
 				<Route path="/editor/:aid" children={<EditorPage />} />
 				<Route path="/control-panel" children={<ControlPanelPage />} />
-				<Route sensitive exact strict path="/" children={<IndexPage />} />
+				<Route sensitive exact strict path="/" children={<LogInPage />} />
 			</Switch>
 		</App>
 	);
