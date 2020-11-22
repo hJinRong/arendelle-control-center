8cfca5b7792427804038b00c7fc177f93b68fa67
wutai <1615140542@qq.com>
2020-11-22 23:29:51 +0800

    simplify

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -6,16 +6,14 @@ import React from 'react';
 
 export default function LogIn() {
 	const history = useHistory();
-	const [account, setAccount] = useState<string | null>(null);
-	const [password, setPassword] = useState<string | null>(null);
+	const [account, setAccount] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
 	const disabled = useMemo(() => {
 		if (
-			account === null ||
-			password === null ||
 			account.trim().length === 0 ||
-			account.indexOf(' ') !== -1 ||
+			account.includes(' ') ||
 			password.trim().length === 0 ||
-			password.indexOf(' ') !== -1
+			!password.includes(' ')
 		) {
 			return true;
 		}
@@ -27,14 +25,13 @@ export default function LogIn() {
 	});
 
 	useEffect(() => {
-		if (localStorage.getItem('token') !== null) {
+		if (!localStorage.getItem('token')) {
 			history.push('/control-panel');
 		}
 	});
 
-	const login = (e: any) => {
+	const login = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
 		e.preventDefault();
-		e.target.blur();
 		axios
 			.post(
 				'https://arendelle.tech/api/auth',
@@ -54,38 +51,36 @@ export default function LogIn() {
 	};
 
 	return (
-			<div className={`login-con`}>
-				<div className={`greeting`}></div>
-				<div className={`login-form`}>
-					<form action="https://arendelle.tech/auth" method="post">
-						<input
-							type="text"
-							name="account"
-							placeholder="Account"
-							autoComplete="on"
-							onChange={(e) => setAccount(e.target.value)}
-						/>
-						<input
-							type="password"
-							name="password"
-							placeholder="Password"
-							autoComplete="current-password"
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-						<button
-							title={
-								disabled
-									? 'Blank option or contains invalid characters '
-									: 'Okay'
-							}
-							disabled={disabled}
-							type="submit"
-							onClick={(e) => login(e)}
-						>
-							Log in
-						</button>
-					</form>
-				</div>
+		<div className={`login-con`}>
+			<div className={`greeting`}></div>
+			<div className={`login-form`}>
+				<form action="https://arendelle.tech/auth" method="post">
+					<input
+						type="text"
+						name="account"
+						placeholder="Account"
+						autoComplete="on"
+						onChange={(e) => setAccount(e.target.value)}
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						autoComplete="current-password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button
+						title={
+							disabled ? 'Blank option or contains invalid characters ' : 'Okay'
+						}
+						disabled={disabled}
+						type="submit"
+						onClick={(e) => login(e)}
+					>
+						Log in
+					</button>
+				</form>
 			</div>
+		</div>
 	);
 }
